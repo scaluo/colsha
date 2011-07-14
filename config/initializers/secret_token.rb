7bc0e3cc684ce1cf81d094aa8706750171aed019
liwenjie <scaluo@gmail.com>
2011-07-14 21:57:48 +0800

    init project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Colsha::Application.config.secret_token = '7755e098630ef106cea18ad8427bdcd0d331a59dcdea1cb9c44423ba08deb3240af95eb8fde4bfaab38ae867f7aab31e97208f38bef317ea08f67f8dcc484edb'
